50528ab91f7e3e004ed329e0daafbd0cc6cbbe21
eldius <eldiosantos@gmail.com>
2026-03-12 19:22:58 -0300

    Starting to create an HTTP server trace instrumentation...

--- http/server/logging.go
+++ http/server/logging.go
@@ -3,7 +3,9 @@ package server
 import (
 	"github.com/eldius/initial-config-go/http/logging"
 	"github.com/eldius/initial-config-go/logs"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -63,4 +65,22 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 
 		log.WithExtraData("request", reqInfo).Info("IncomingHTTPRequestResponse")
 	})
+}
+
+// TelemetryMiddleware is a middleware that wraps the provided http.ServeMux with OpenTelemetry instrumentation.
+// It instruments the ServeMux with the otelhttp.NewHandler function and enables logging for all requests handled by the ServeMux.
+func TelemetryMiddleware(mux *http.ServeMux) http.Handler {
+	return otelhttp.NewHandler(
+		LoggingMiddleware(mux),
+		"api-call-received",
+		otelhttp.WithSpanNameFormatter(func(operation string, r *http.Request) string {
+			pattern := r.Pattern
+			if pattern == "" {
+				return r.Method + " " + r.URL.Path
+			}
+			if strings.HasPrefix(pattern, r.Method+" ") {
+				return pattern
+			}
+			return r.Method + " " + pattern
+		}))
 }
